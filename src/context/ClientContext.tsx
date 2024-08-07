a1d20225fcd31cbcc3d2f1d8c84c98efe1add4ce
Eric <eriicsilva28@gmail.com.br>
2024-08-07 09:43:37 -0300

    refactor: refatoração do context

---
+++ src/context/ClientContext.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-sparse-arrays */
+import React, { createContext, useState } from 'react'
+import { ClientContextType, ClientProviderProps } from '../types/TContext'
+import { DataValuesType, IDataForm } from '../types/TForm'
+
+export const ClientContext = createContext<ClientContextType>(
+  {} as ClientContextType
+)
+
+export const ClientProvider = ({ children }: ClientProviderProps) => {
+  const [currentStep, setStepForm] = useState<number>(1)
+  const [data, setData] = useState<IDataForm>({} as IDataForm)
+
+  const nextStep = () => setStepForm((prev) => prev + 1)
+  const prevStep = () => setStepForm((prev) => prev - 1)
+
+  const setFieldValue = React.useCallback(
+    (section: keyof IDataForm, value: DataValuesType) => {
+      setData((prevData) => ({
+        ...prevData,
+        [section]: value
+      }))
+    },
+    []
+  )
+
+  const editField = React.useCallback(
+    (section: keyof IDataForm, field: string, value: DataValuesType) => {
+      setData((prevData) => ({
+        ...prevData,
+        [section]: {
+          ...prevData[section],
+          [field]: value
+        }
+      }))
+    },
+    []
+  )
+
+  // able to edit also
+  // const setFieldValue = (section: keyof IDataForm, value: DataValuesType) => {
+  //   setData((prevData) => ({
+  //     ...prevData,
+  //     [section]: {
+  //       ...prevData[section],
+  //       ...value
+  //     }
+  //   }))
+  // }
+
+  const contextValue = React.useMemo(
+    () => ({
+      data,
+      currentStep,
+      nextStep,
+      prevStep,
+      setFieldValue,
+      editField
+    }),
+    [currentStep, , setFieldValue]
+  )
+
+  return (
+    <ClientContext.Provider value={contextValue}>
+      {children}
+    </ClientContext.Provider>
+  )
+}
